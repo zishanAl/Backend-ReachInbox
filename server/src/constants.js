b0fb4e24f178da2c114c1cae5f67733a230dc2d4
zishanAl <zishanalam145@gmail.com>
2024-11-07 15:45:33 +0530

    auth error

--- server/src/constants.js
+++ server/src/constants.js
@@ -2,7 +2,7 @@ require('dotenv');
 
 const auth = {
     type: 'OAuth2',
-    user: 'aman.2101010cs@iiitbh.ac.in',
+    user: 'md.2101013cs@iiitbh.ac.in',
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     refreshToken: process.env.GOOGLE_REFRESH_TOKEN
